1510fe137990a70194750728796c496feabca060
Julian K. Arni <jkarni@gmail.com>
2015-12-17 17:54:57 +0100

    add package

--- run.sh
+++ run.sh
@@ -3,6 +3,6 @@ set -o errexit
 OUT="quine.html"
 
 while true ; do
-    stack --resolver lts-3.14 --install-ghc runghc $OUT | sponge $OUT
+    stack --resolver lts-3.14 --install-ghc runghc --package random $OUT | sponge $OUT
     sleep 5
 done
